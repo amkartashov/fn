d60004656c701fe4821b7e0c86dd75cdb28b2db0
Andrey Kartashov <gorilych@gmail.com>
2018-02-23 19:53:03 +0700

    main() now runs Telegram Bot UI

--- main.go
+++ main.go
@@ -2,41 +2,55 @@ package main
 
 import (
 	"fmt"
-	//news "github.com/gorilych/fn/news"
-	//aRepo "github.com/gorilych/fn/news/articlerepo/sqlite"
-	httpSrc "github.com/gorilych/fn/news/source/http"
-	rssSrc "github.com/gorilych/fn/news/source/rss"
+	ui "github.com/gorilych/fn/news/ui/telegram"
+	// httpSrc "github.com/gorilych/fn/news/source/http"
+	// rssSrc "github.com/gorilych/fn/news/source/rss"
+	aRepo "github.com/gorilych/fn/news/articlerepo/sqlite"
+	//"time"
+  "os"
 )
 
 func main() {
 	//var a = news.Article{Title: "Lorem Ipsum", Link: "https:/example.com" }
-	//repo, err := aRepo.NewSqliteArticleRepo("./articles.db")
-	//if err != nil { fmt.Println(err) }
-	//fmt.Println(a)
-	//if err == nil {
-	//  repo.StoreArticle(a)
-	//}
-	//as, err := repo.GetArticlesBy("")
-	//if err != nil { fmt.Println(err) }
-	//fmt.Println(as)
-	rs, err := rssSrc.NewRssNewsSource("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
+  token := os.Getenv("TGBOTTOKEN")
+	repo, err := aRepo.NewSqliteArticleRepo("./articles.db")
 	if err != nil {
 		fmt.Println(err)
 	}
-	as, _ := rs.FetchAllArticles()
-	fmt.Println(as)
-	hs, err := httpSrc.NewHttpNewsSource("https://lenta.ru/parts/news")
-	if err != nil {
-		fmt.Println(err)
-	}
-	hs.SetParser(map[string]string{
-		"item":  ".item.news",
-		"link":  ".titles a",
-		"title": ".titles a",
-	})
-	as, err = hs.FetchAllArticles()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(as)
+
+	ui.Run(repo, token)
+	// rs, err := rssSrc.NewRssNewsSource("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
+	// if err != nil {
+	//   fmt.Println(err)
+	// }
+	// as, _ := rs.FetchAllArticles()
+	// fmt.Println("============ Articles in RSS")
+	// fmt.Println(len(as))
+	// hs, err := httpSrc.NewHttpNewsSource("https://lenta.ru/parts/news")
+	// if err != nil {
+	//   fmt.Println(err)
+	// }
+	// hs.SetParser(map[string]string{
+	//   "item":  ".item.news",
+	//   "link":  ".titles a",
+	//   "title": ".titles a",
+	// })
+	// as, err = hs.FetchAllArticles()
+	// if err != nil {
+	//   fmt.Println(err)
+	// }
+	// fmt.Println("============ Articles in HTML")
+	// fmt.Println(len(as))
+	// newsSrv := news.NewsService(repo, 2*time.Second)
+	// fmt.Println("============ Started service")
+	// newsSrv.AddSource(rs)
+	// fmt.Println("============ Added RSS, now wait")
+	// time.Sleep(3 * time.Second)
+	// as, _ = repo.GetArticlesBy("")
+	// fmt.Println(len(as))
+	// newsSrv.AddSource(hs)
+	// fmt.Println("============ Added HTTP, now wait")
+	// time.Sleep(3 * time.Second)
+	// as, _ = repo.GetArticlesBy("")
+	// fmt.Println(len(as))
 }
